ce8e4632041f2a474b2c6794a0e4cca67d484383
Michael Genson <genson.michael@gmail.com>
2026-07-13 10:26:50 -0500

    infer banner if absent

--- models/jewel.py
+++ models/jewel.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from typing import Self
+from urllib.parse import urlparse
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class JewelOfferStatus(Enum):
@@ -31,5 +33,19 @@ class JewelUserConfig(BaseModel):
     password: str
     device_token: str
     store_ids: list[str]
+
     root: str = "https://www.jewelosco.com"
-    banner: str = "jewelosco"
+    banner: str = ""
+
+    @model_validator(mode="after")
+    def validate_banner(self) -> Self:
+        if not self.banner:
+            # infer banner from root
+            try:
+                hostname = urlparse(self.root).hostname or ""
+                labels = hostname.split(".")
+                self.banner = labels[-2]
+            except Exception as e:
+                raise ValueError("Unable to infer banner from root, are you sure root is correct?") from e
+
+        return self
